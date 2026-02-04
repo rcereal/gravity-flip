3275d22438ddaa36e1cde6c4d144d8b4976690c8
Ryan Vitor <ryanvitor330@gmail.com>
2026-02-04 18:27:48 -0300

    feat: configuracao inicial e limpeza do projeto

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <h1 style={{ color: "white", textAlign: "center", marginTop: "20%" }}>
+        Gravity Flip - Setup Pronto
+      </h1>
+    </main>
+  );
+}
